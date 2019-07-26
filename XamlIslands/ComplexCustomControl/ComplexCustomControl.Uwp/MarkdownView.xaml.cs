9b449140cf184f8a1489f0811734a5b9a3d94750
Matteo Pagani <info@qmatteoq.com>
2019-07-26 18:22:38 +0200

    Added new XAML Islands sample

---
+++ XamlIslands/ComplexCustomControl/ComplexCustomControl.Uwp/MarkdownView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ComplexCustomControl.Uwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MarkdownView : UserControl
+    {
+        public MarkdownView()
+        {
+            this.InitializeComponent();
+            this.Markdown.Text = MarkdownText;
+        }
+
+        public string MarkdownText
+        {
+            get { return (string)GetValue(MarkdownTextProperty); }
+            set { SetValue(MarkdownTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MarkdownText.  This enables animation, styling, binding, etc...
+        private static readonly DependencyProperty MarkdownTextProperty =
+            DependencyProperty.Register("MarkdownText", typeof(string), typeof(MarkdownView), new PropertyMetadata(string.Empty, (obj, args) =>
+                {
+                    var control = obj as MarkdownView;
+                    var markdown = control.FindName("Markdown") as MarkdownTextBlock;
+                    markdown.Text = args.NewValue.ToString();
+                }
+            ));
+    }
+}
---
+++ XamlIslands/ComplexCustomControl/ComplexCustomControl.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using ComplexCustomControl.Uwp;
+using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
+using System.Windows;
+
+namespace ComplexCustomControl.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsXamlHost_ChildChanged(object sender, EventArgs e)
+        {
+            if (sender is WindowsXamlHost host && host.Child is MarkdownView markdownView)
+            {
+                markdownView.MarkdownText = "This is **markdown** rendered by the UWP control included in the [Windows Community Toolkit](https://docs.microsoft.com/en-us/windows/communitytoolkit/)";
+            }
+        }
+    }
+}
