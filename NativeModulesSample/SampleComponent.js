6f31537a8615bf845b1f2fe931418da41ff87573
Matteo Pagani <matteo.pagani@microsoft.com>
2019-12-16 14:44:07 +0100

    Updated JavaScript wrapper

--- NativeModulesSample/SampleComponent.js
+++ NativeModulesSample/SampleComponent.js
@@ -1,18 +1,5 @@
 import { NativeModules } from 'react-native';
 
-export const getResult = () => {
-    return new Promise ((resolve, reject) => {
-        NativeModules.SampleComponent.getResult(function(result, error) {
-            if (error) {
-                return reject(error);
-            }
-            else {
-                resolve(result);
-            }
-        })
-    })
-}
-
 export const getDeviceModel = () => {
     return new Promise((resolve, reject) => {
         NativeModules.SampleComponent.getDeviceModel(function(result, error) {
@@ -24,8 +11,4 @@ export const getDeviceModel = () => {
             }
         })
     })
-}
-
-export const doSomething = () => {
-    return 'Hello world'
 }
