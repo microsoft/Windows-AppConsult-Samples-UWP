963094d9cdbac0c08a6eb5d8538f97474fa6e8e4
Sebastien Bovo <sbovo@microsoft.com>
2020-06-06 09:06:55 +0200

    Anchor code modification

--- UWP-Advanced-Inking/XAMLShapesAdvancedManipulations/XAMLShapesAdvancedManipulations/MainPage.xaml.cs
+++ UWP-Advanced-Inking/XAMLShapesAdvancedManipulations/XAMLShapesAdvancedManipulations/MainPage.xaml.cs
@@ -27,8 +27,8 @@ using Path = Windows.UI.Xaml.Shapes.Path;
 
 namespace XAMLShapesAdvancedManipulations
 {
-    public sealed partial class MainPage : Page
-    {
+	public sealed partial class MainPage : Page
+	{
 		public MainPage()
 		{
 			this.InitializeComponent();
@@ -71,10 +71,14 @@ namespace XAMLShapesAdvancedManipulations
 			line.X2 = stroke.GetInkPoints().Last().Position.X;
 			line.Y2 = stroke.GetInkPoints().Last().Position.Y;
 
+			// Draw the anchors and initiate the advanced drag & drop functionalities
 			line.Tapped += Line_Tapped;
 
 			return line;
 		}
+
+		// XAML Shapes manipulations
+		private TranslateTransform dragTranslation;
 
 		private void Line_Tapped(object sender, TappedRoutedEventArgs e)
 		{
@@ -85,75 +89,171 @@ namespace XAMLShapesAdvancedManipulations
 			Line line = (Line)sender;
 			line.Stroke = new SolidColorBrush(Windows.UI.Colors.DarkRed);
 			line.StrokeThickness = 10;
-			line.StrokeStartLineCap = PenLineCap.Triangle;
-			line.StrokeEndLineCap = PenLineCap.Triangle;
+			//line.StrokeStartLineCap = PenLineCap.Triangle;
+			//line.StrokeEndLineCap = PenLineCap.Triangle;
 
 
-			int size_EndLines = 30;
-			//TODO: [PoC] - Write some 'anchors/objects' to be able to modify the lenght of the line
-			Ellipse e1 = new Ellipse
+			int size_EndLines = 25;
+			// Create 2 circles for the ends of the line
+			Ellipse anchorOrigin = new Ellipse
 			{
 				Fill = new SolidColorBrush(Windows.UI.Colors.OrangeRed),
 				Height = size_EndLines,
 				Width = size_EndLines
 			};
+			ShapesCanvas.Children.Add(anchorOrigin);
 
-			ShapesCanvas.Children.Add(e1);
-
-
-
-
-
-			Ellipse e2 = new Ellipse
+			Ellipse anchorEnd = new Ellipse
 			{
 				Fill = new SolidColorBrush(Windows.UI.Colors.OrangeRed),
 				Height = size_EndLines,
 				Width = size_EndLines
 			};
+			ShapesCanvas.Children.Add(anchorEnd);
 
-			ShapesCanvas.Children.Add(e2);
-
-
-
-			Canvas.SetLeft(e1, line.X1 - size_EndLines / 2);
-			Canvas.SetLeft(e2, line.X2 - size_EndLines / 2);
-			Canvas.SetTop(e1, line.Y1 - size_EndLines / 2);
-			Canvas.SetTop(e2, line.Y2 - size_EndLines / 2);
+			// Put the anchors at the origin and at the end of the line
+			Canvas.SetLeft(anchorOrigin, line.X1 - size_EndLines / 2);
+			Canvas.SetLeft(anchorEnd, line.X2 - size_EndLines / 2);
+			Canvas.SetTop(anchorOrigin, line.Y1 - size_EndLines / 2);
+			Canvas.SetTop(anchorEnd, line.Y2 - size_EndLines / 2);
 
 
-			// Enable manipulation on the anchors
-			//e1.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
-			//e1.ManipulationStarted += E1_ManipulationStarted;
-			//e1.ManipulationDelta += E1_ManipulationDelta;
-			//e1.ManipulationCompleted += E1_ManipulationCompleted;
+			// Enable manipulations on the anchors
+			anchorOrigin.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+			anchorOrigin.ManipulationStarted += Anchor_ManipulationStarted;
+			anchorOrigin.ManipulationDelta += Anchor_Origin_ManipulationDelta;
+			anchorOrigin.ManipulationCompleted += Anchor_Origin_ManipulationCompleted;
 
-			//e2.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
-			//e2.ManipulationStarted += E2_ManipulationStarted;
-			//e2.ManipulationDelta += E2_ManipulationDelta;
-			//e2.ManipulationCompleted += E2_ManipulationCompleted;
+			anchorEnd.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+			anchorEnd.ManipulationStarted += Anchor_ManipulationStarted;
+			anchorEnd.ManipulationDelta += Anchor_End_ManipulationDelta;
+			anchorEnd.ManipulationCompleted += Anchor_End_ManipulationCompleted;
 
 
-			// Take this line as the one to be modified with the anchors
-			activeLine.line = line;
-			activeLine.E1 = e1;
-			activeLine.E2 = e2;
+			InitializeActiveLine(line, anchorOrigin, anchorEnd);
+		}
+
+		private void Anchor_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+		{
+			Ellipse anchor = (Ellipse)sender;
+
+			// Initialize the transforms that will be used to manipulate the shape
+			dragTranslation = new TranslateTransform();
+			anchor.RenderTransform = dragTranslation;
+			anchor.Fill = new SolidColorBrush(Windows.UI.Colors.Orange);
+		}
+
+		private void Anchor_Origin_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+		{
+			AnchorManipulationDelta(sender, e, true);
+		}
+
+		private void Anchor_End_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+		{
+			AnchorManipulationDelta(sender, e, false);
 		}
 
 
+		private void AnchorManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e,
+			bool OriginofLine)
+		{
+			double x = e.Delta.Translation.X;
+			double y = e.Delta.Translation.Y;
+			dragTranslation.X += x;
+			dragTranslation.Y += y;
 
+			if (OriginofLine)
+			{
+				activeLine.line.X1 += x;
+				activeLine.line.Y1 += y;
+			}
+			else
+			{
+				activeLine.line.X2 += x;
+				activeLine.line.Y2 += y;
+			}
+		}
+
+		private void Anchor_Origin_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+		{
+			AnchorManipulationCompleted(sender, e, true);
+		}
+
+		private void Anchor_End_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+		{
+			AnchorManipulationCompleted(sender, e, false);
+		}
+
+
+		private void AnchorManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e, bool OriginofLine)
+		{
+			Ellipse el = (Ellipse)sender;
+			el.RenderTransform = null;
+			double x = e.Cumulative.Translation.X;
+			double y = e.Cumulative.Translation.Y;
+			Canvas.SetLeft(el, Canvas.GetLeft(el) + x);
+			Canvas.SetTop(el, Canvas.GetTop(el) + y);
+
+			el.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+
+			if (OriginofLine)
+			{
+				activeLine.line.X1 = activeLine.initialX1 + x;
+				activeLine.line.Y1 = activeLine.initialY1 + y;
+
+				activeLine.initialX1 = activeLine.line.X1;
+				activeLine.initialY1 = activeLine.line.Y1;
+			}
+			else
+			{
+				activeLine.line.X2 = activeLine.initialX2 + x;
+				activeLine.line.Y2 = activeLine.initialY2 + y;
+
+				activeLine.initialX2 = activeLine.line.X2;
+				activeLine.initialY2 = activeLine.line.Y2;
+			}
+		}
+
+		/// <summary>
+		/// Take this line as the one to be modified with the anchors
+		/// Selecting the line is:
+		///   - get the line object
+		///   - keep track of its coordinates
+		///   - get the 2 anchors objects
+		/// </summary>
+		private void InitializeActiveLine(Line line, Ellipse origin, Ellipse end)
+		{
+			activeLine.line = line;
+			activeLine.initialX1 = line.X1;
+			activeLine.initialY1 = line.Y1;
+			activeLine.initialX2 = line.X2;
+			activeLine.initialY2 = line.Y2;
+			activeLine.AnchorOrigin = origin;
+			activeLine.AnchorEnd = end;
+		}
+
+
+		/// <summary>
+		/// Before doing manipulations on the newly selected line, we put back the origina color
+		/// of the previous line and we remove its the anchors from the screen
+		/// </summary>
 		public void UnselectActiveLine()
 		{
 			if (activeLine.line != null)
 			{
 				activeLine.line.Stroke = new SolidColorBrush(Windows.UI.Colors.Green);
 				activeLine.line.StrokeThickness = 6;
-				ShapesCanvas.Children.Remove(activeLine.E1);
-				ShapesCanvas.Children.Remove(activeLine.E2);
-				ShapesCanvas.Children.Remove(activeLine.CenterEllipse);
-				activeLine.bezierPath = null;
+				ShapesCanvas.Children.Remove(activeLine.AnchorOrigin);
+				ShapesCanvas.Children.Remove(activeLine.AnchorEnd);
 			}
 		}
 
+		/// <summary>
+		/// We doing manipulations on the anchors of a line, we move the line position
+		/// in real time, so we have to keep track about the line's original position
+		/// in order to do the definitive move when the manipulation is completed.
+		/// We also move the anchors during the manipulation.
+		/// </summary>
 		public struct ActiveLine
 		{
 			public Line line;
@@ -161,13 +261,10 @@ namespace XAMLShapesAdvancedManipulations
 			public double initialY1;
 			public double initialX2;
 			public double initialY2;
-			public Ellipse E1;
-			public Ellipse E2;
-			public Ellipse CenterEllipse;
-			public double initialCenterEllipseX;
-			public double initialCenterEllipseY;
-			public Path bezierPath;
+			public Ellipse AnchorOrigin;
+			public Ellipse AnchorEnd;
 		}
+
 		public ActiveLine activeLine;
 	}
 }
