23c6a39dd726e374e13ac361c39608db6dd1910c
Matteo Pagani <matteo.pagani@microsoft.com>
2020-06-07 14:31:38 +0200

    Added Geolocation Sample

---
+++ GeolocationSample/windows/GeolocationSample/ReactPackageProvider.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "ReactPackageProvider.h"
+
+#include "NativeModules.h"
+
+
+
+using namespace winrt::Microsoft::ReactNative;
+
+namespace winrt::GeolocationSample::implementation
+{
+
+void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept
+{
+    AddAttributedModules(packageBuilder);
+}
+
+} // namespace winrt::GeolocationSample::implementation
+
+
